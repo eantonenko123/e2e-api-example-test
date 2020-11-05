dd92f09bf6209f9375d56bb25be32029b13736fd
Elena Antonenko <e.antonenko@whirl.sg>
2020-11-05 20:34:02 +0200

    scenario outline cucumber

--- features/step_definitions/userCRUD_SD.js
+++ features/step_definitions/userCRUD_SD.js
@@ -8,29 +8,50 @@ const expect = require('chai').expect;
 const user = userData.getUser();
 const updateUser = userData.getUser({ name: "morpheus updated"});
 
-When('user create', async function () {
-    this.res = await userHelper.createNewUser(agent, user);
-    this.userId = this.res.body.id;
+// When('user create', async function () {
+//     this.res = await userHelper.createNewUser(agent, user);
+//     this.userId = this.res.body.id;
+// });
+//
+// Then('check that user was created', function () {
+//     expect(this.res.statusCode).to.equal(201);
+//     expect(this.res.body.name).to.equal(user.name);
+// });
+//
+// When('user updated', async function () {
+//     this.res = await userHelper.updateUser(agent, updateUser, this.userId);
+// });
+//
+// Then('check that user was updated', function () {
+//     expect(this.res.statusCode).to.equal(200);
+//     expect(this.res.body.name).to.equal(updateUser.name);
+// });
+//
+// When('user delete', async function () {
+//     this.res = await userHelper.deleteUser(agent, this.userId);
+// });
+//
+// Then('check that user was deleted', function () {
+//     expect(this.res.statusCode).to.equal(204);
+// });
+
+When('user {string}', async function (action) {
+    switch (action) {
+        case 'create':
+            this.res = await userHelper.createNewUser(agent, user);
+            this.userId = this.res.body.id;
+            break;
+        case 'update':
+            this.res = await userHelper.updateUser(agent, updateUser, this.userId);
+            break;
+        case 'delete':
+            this.res = await userHelper.deleteUser(agent, this.userId);
+            break;
+    }
 });
 
-Then('check that user was created', function () {
-    expect(this.res.statusCode).to.equal(201);
-    expect(this.res.body.name).to.equal(user.name);
-});
-
-When('user updated', async function () {
-    this.res = await userHelper.updateUser(agent, updateUser, this.userId);
-});
-
-Then('check that user was updated', function () {
-    expect(this.res.statusCode).to.equal(200);
-    expect(this.res.body.name).to.equal(updateUser.name);
-});
-
-When('user delete', async function () {
-    this.res = await userHelper.deleteUser(agent, this.userId);
-});
-
-Then('check that user was deleted', function () {
-    expect(this.res.statusCode).to.equal(204);
+Then('check that user was {string}', function (status) {
+    console.log("status", status)
+    console.log("this.res.statusCode", this.res.statusCode)
+    expect(this.res.statusCode).to.equal(status);
 });
--- features/userCRUD.feature
+++ features/userCRUD.feature
@@ -1,13 +1,24 @@
 Feature: User CRUD
 
-  Scenario: User create
-    When user create
-    Then check that user was created
+#  Scenario: User create
+#    When user create
+#    Then check that user was created
+#
+#  Scenario: User update
+#    When user updated
+#    Then check that user was updated
+#
+#  Scenario: User delete
+#    When user delete
+#    Then check that user was deleted
 
-  Scenario: User update
-    When user updated
-    Then check that user was updated
 
-  Scenario: User delete
-    When user delete
-    Then check that user was deleted
+  Scenario Outline: user CRUD
+    When user "<action>"
+    Then check that user was "<status>"
+
+    Examples:
+      | action  | status |
+      | create  | 201    |
+      | update  | 200    |
+      | delete  | 204    |
